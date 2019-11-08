38ac5f37981db34ee16b34a3af2d19a694caa546
deezy13 <deezy13@gmail.com>
2019-11-08 17:36:30 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,6 +3,7 @@ class Author
   attr_accessor :name, :posts
   
   @@post_count = 1 
+  
   def initialize(name)
     @name = name 
     @posts = []
@@ -16,6 +17,7 @@ class Author
     
   def add_post_by_title(title)
     post = Post.new(title)
+    @post << post
     post.author = self 
     @@post_count +=1
   end 
@@ -27,4 +29,5 @@ end
 def post_count
   @@post_count
 end 
+
 end 
