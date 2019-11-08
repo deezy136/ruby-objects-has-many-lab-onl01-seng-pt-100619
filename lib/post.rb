cead73a5a44911c476e9bb05e0238cfbdcb8585c
deezy13 <deezy13@gmail.com>
2019-11-08 17:01:03 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,30 @@
+class Post 
+  
+  attr_accessor :title, :author, :post
+  
+@@post_count = 1
+  @@all = []
+  def initialize(title)
+    @title = title
+    @posts = []
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end
+  
+  def add_post(post)
+       @post << post
+        post.author = self
+     end
+
+
+  def author_name
+    if self.author
+      self.author.title
+    else
+      nil
+    end
+  end
+end 
