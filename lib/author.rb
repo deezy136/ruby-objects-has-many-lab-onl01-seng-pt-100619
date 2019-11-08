d1e83e3230609cbfecaa9e25ecaeddc7dc141362
deezy13 <deezy13@gmail.com>
2019-11-08 17:33:15 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -8,12 +8,14 @@ class Author
   end 
   
   def add_post(post)
-    
-    @posts << post
+        @posts << post
       post.author = self
   end 
     
-  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self 
+  end 
     
   def posts
     Post.all.select { |post| post.author == self }
